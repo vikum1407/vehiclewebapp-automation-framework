0d58bb419d93449aeab1724d6b4ce0394fe693ae
vikum1407 <vikum1407@gmail.com>
2024-03-31 22:50:42 +0530

    developed first page test cases

---
+++ tests/SafetyHighlightsTest.spec.js
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+import { VolvoCommonUtilMethods } from '../util/VolvoCommonUtilMetods';
+import { SafetyHighlightsPage } from '../pages/SafetyHighlightsPage';
+
+    
+
+test('Validate highlight main image and description', async ({ page }) => {
+
+    const volvoCommonUtil = new VolvoCommonUtilMethods(page);
+    const safetyHighlightsPage = new SafetyHighlightsPage(page);     
+
+    await volvoCommonUtil.gotoPage();
+    await safetyHighlightsPage.validateFirstDescription();
+    await safetyHighlightsPage.validateMainImage();
+    await safetyHighlightsPage.validateAim();
+});
+
+test('Validate safety for drive', async ({ page }) => {
+
+    const volvoCommonUtil = new VolvoCommonUtilMethods(page);
+    const safetyHighlightsPage = new SafetyHighlightsPage(page);     
+
+    await volvoCommonUtil.gotoPage();
+    await safetyHighlightsPage.validateSafeSpace();
+    await safetyHighlightsPage.validateSafeSpaceTitle();
+    await safetyHighlightsPage.validateFirstDescription();
+    await safetyHighlightsPage.validateMainImage();
+    
+    //await safetyHighlightsPage.clickSafeAcclaimed();
+    await safetyHighlightsPage.validateSafeAcclaimed();
+    await safetyHighlightsPage.validateSafeAcclaimedTitle();
+    await safetyHighlightsPage.validateSafeAcclaimedDescription();
+    await safetyHighlightsPage.validateSafeAcclaimedImage();
+
+    await safetyHighlightsPage.validateDBResearchButton();
+    await safetyHighlightsPage.validateDBResearchTitle();
+    await safetyHighlightsPage.validateDBResearchDescription();
+    await safetyHighlightsPage.validateDBResearchImage();
+
+    await safetyHighlightsPage.validateZeroColTitle();
+    await safetyHighlightsPage.validateFirstDescription();
+    await safetyHighlightsPage.validateMainImage();
+});
+
+test('Validate our learnings', async ({ page }) => {
+
+    const volvoCommonUtil = new VolvoCommonUtilMethods(page);
+    const safetyHighlightsPage = new SafetyHighlightsPage(page);     
+
+    await volvoCommonUtil.gotoPage();
+    await safetyHighlightsPage.validateEvolutionTitle();
+    await safetyHighlightsPage.validateEvolutionDesc();
+    await safetyHighlightsPage.validateEvolutionImage();
+
+    await safetyHighlightsPage.validateAutonomousDTitle();
+    await safetyHighlightsPage.validateAutonomousDDesc();
+    await safetyHighlightsPage.validateAutonomousDImage();
+
+    await safetyHighlightsPage.validateRechargeVehicleTitle();
+    await safetyHighlightsPage.validateNextslideBtn();
+    await safetyHighlightsPage.validateAutonomousDImage();
+    await safetyHighlightsPage.validateEC40Button();
+    await safetyHighlightsPage.validateBackToTopButton();
+    
+    
+});
