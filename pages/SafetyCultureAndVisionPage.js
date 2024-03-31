28d8f402a98a8e4dc8ce4f43c0e4e9adb7d635c8
vikum1407 <vikum1407@gmail.com>
2024-03-31 22:49:32 +0530

    developed methods

--- pages/SafetyCultureAndVisionPage.js
+++ pages/SafetyCultureAndVisionPage.js
@@ -7,26 +7,108 @@ class safetyCultureAndVisionPage
     constructor(page)
     {
         this.page = page;
-        visonMainDescription = page.locator('#ModelIntro-2');
-        visonImg = page.getByRole('img', { name: 'Three people having a chat' });
-        cofounderSaying = page.locator('#ImageWithText-1');
-        cofounderImg = page.getByRole('img', { name: 'A vintage shot of two' });
-        safetyDNATitle = page.locator('#ImageWithText-2');
-        safetyDNADescription = page.locator('#ImageWithText-2');
-        futureSafetyTitle = page.locator('#ModelIntro-3');
-        futureSafetyDescription = page.locator('#ModelIntro-3');
-        zeroCollisionsTitle = page.locator('#MediaHighlights-1');
-        zeroCollisionsDescription = page.locator('#MediaHighlights-1');
-        zeroCollisionsImg = page.getByRole('img', { name: 'Digital rendering of two cars' });
-        worldOfDataTitle = page.locator('#MediaHighlights-1');
-        worldOfDataDescription = page.locator('#MediaHighlights-1');
-        worldOfDataImg =  page.getByRole('img', { name: 'A man inspecting a crash-test' });
-        pioneeringInnovationsTitle = page.locator('#TextStatement-1');
-        pioneeringInnovationsImg = page.getByText('Ideas that change the world');
-        pioneeringInnovationsDesc = page.locator('#ModelIntro-4');
-        supportTitle = page.locator('#ImageWithText-3');
-        supportDescription = page.locator('#ImageWithText-3');
-        supportImg = page.getByRole('img', { name: 'A digital rendering of a' });
+        this.visonMainDescription = this.page.locator('#ModelIntro-2');
+        this.visonImg = this.page.getByRole('img', { name: 'Three people having a chat' });
+        this.cofounderSaying = this.page.locator('#ImageWithText-1');
+        this.cofounderImg = this.page.getByRole('img', { name: 'A vintage shot of two' });
+        this.safetyDNATitle = this.page.locator('#ImageWithText-2');
+        this.safetyDNADescription = this.page.locator('#ImageWithText-2');
+        this.futureSafetyTitle = this.page.locator('#ModelIntro-3');
+        this.futureSafetyDescription = this.page.locator('#ModelIntro-3');
+        this.zeroCollisionsTitle = this.page.locator('#MediaHighlights-1');
+        this.zeroCollisionsDescription = this.page.locator('#MediaHighlights-1');
+        this.zeroCollisionsImg = this.page.getByRole('img', { name: 'Digital rendering of two cars' });
+        this.worldOfDataTitle = this.page.locator('#MediaHighlights-1');
+        this.worldOfDataDescription = this.page.locator('#MediaHighlights-1');
+        this.worldOfDataImg =  this.page.getByRole('img', { name: 'A man inspecting a crash-test' });
+        this.pioneeringInnovationsTitle = this.page.locator('#TextStatement-1');
+        this.pioneeringInnovationsImg = this.page.getByText('Ideas that change the world');
+        this.pioneeringInnovationsDesc = this.page.locator('#ModelIntro-4');
+        this.supportTitle = this.page.locator('#ImageWithText-3');
+        this.supportDescription = this.page.locator('#ImageWithText-3');
+        this.supportImg = this.page.getByRole('img', { name: 'A digital rendering of a' });
+    }
+
+    async validatevisonMainDescription(){
+        await expect(this.visonMainDescription).toContainText(testData.descriptionText);
+    }
+
+    async validateVisionImage(){
+        await expect(this.visonImg).toBeVisible();
+    }
+    
+    async validateCofounderSaying(){
+        await expect(this.cofounderSaying).toContainText(testData.cofounderSaying);
+    }
+
+    async validateCofounderImg(){
+        await expect(this.cofounderImg).toBeVisible();
+    }
+
+    async validateSafetyDNATitle(){
+        await expect(this.safetyDNATitle).toContainText(testData.safetyDNATitle);
+    }
+
+    async validateSafetyDNADescription(){
+        await expect(this.safetyDNADescription).toContainText(testData.safetyDNADescription);
+    }
+
+    async validateFutureSafetyTitle(){
+        await expect(this.futureSafetyTitle).toContainText(testData.futureSafetyTitle);
+    }
+
+    //-----------------
+
+    async validateFutureSafetyDescription(){
+        await expect(this.futureSafetyDescription).toContainText(testData.futureSafetyDescription);
+    }
+
+    async validateZeroCollisionsTitle(){
+        await expect(this.zeroCollisionsTitle).toContainText(testData.zeroCollisionsTitle);
+    }
+
+    async validateZeroCollisionsDescription(){
+        await expect(this.zeroCollisionsDescription).toContainText(testData.zeroCollisionsDescription);
+    }
+
+    async validateZeroCollisionsImg(){
+        await expect(this.zeroCollisionsImg).toBeVisible();
+    }
+
+    async validateWorldOfDataTitle(){
+        await expect(this.worldOfDataTitle).toContainText(testData.worldOfDataTitle);
+    }
+
+    async validateWorldOfDataDescription(){
+        await expect(this.worldOfDataDescription).toContainText(testData.worldOfDataDescription);
+    }
+
+    async validateWorldOfDataImg(){
+        await expect(this.zeroCollisionsImg).toBeVisible();
+    }
+    
+    async validatePioneeringInnovationsTitle(){
+        await expect(this.pioneeringInnovationsTitle).toContainText(testData.pioneeringInnovationsTitle);
+    }
+
+    async validatePioneeringInnovationsDesc(){
+        await expect(this.pioneeringInnovationsDesc).toContainText(testData.pioneeringInnovationsDesc);
+    }
+
+    async validatePioneeringInnovationsImg(){
+        await expect(this.pioneeringInnovationsImg).toBeVisible();
+    }
+
+    async validateSupportTitle(){
+        await expect(this.supportTitle).toContainText(testData.supportTitle);
+    }
+
+    async validateSupportDescription(){
+        await expect(this.supportDescription).toContainText(testData.supportDescription);
+    }
+
+    async validateSupportImg(){
+        await expect(this.supportImg).toBeVisible();
     }
 
 }
--- util/VolvoCommonUtilMetods.js
+++ util/VolvoCommonUtilMetods.js
@@ -1,4 +1,4 @@
-const {test, expect} = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 const exp = require('constants');
 
 exports.VolvoCommonUtilMethods = 
@@ -10,19 +10,65 @@ class volvoCommonUtilMethods
         this.page = page;
     }
 
-    static async gotoPage(){
-        await page.goto('https://www.volvocars.com/intl/v/car-safety/a-million-more')
+    async gotoPage() {
+        try {
+            await this.page.goto('https://www.volvocars.com/intl/v/car-safety/a-million-more');
+        } catch (error) {
+            console.error('Error navigating to the page:', error);
+        }
     }
 
-    static async descriptionValidation(element, value){
-        await expect(this.page.locator(element)).toHaveText(value);
+    async textContainValidation(element, value){
+        try {
+            if (!this.page) {
+                throw new Error('Page object is not defined.');
+            }
+            await expect(this.page.locator(element)).toContainText(value);
+        } catch (error) {
+            console.error('Error validating text:', error);
+        }
     }
 
-    static async photoValidation(element, value){
+    async clickRejectCookies(){
+        await page.getByRole('button', { name: 'Reject All' }).click();
+    }
+
+    async textFullValidation(element, value){
+        try {
+            if (!this.page) {
+                throw new Error('Page object is not defined.');
+            }
+            await expect(this.page.locator(element)).toHaveText(value);
+        } catch (error) {
+            console.error('Error validating text:', error);
+        }
+    }
+
+    async imageValidation(element){
+        try {
+            if (!this.page) {
+                throw new Error('Page object is not defined.');
+            }
+            await expect(this.page.locator(element)).toBeVisible();
+        } catch (error) {
+            console.error('Error validating image:', error);
+        }
+    }
+
+    async videoValidation(element, value){
         await expect(this.page.locator(element)).toBeVisible(value);
     }
 
-    static async videoValidation(element, value){
-        await expect(this.page.locator(element)).toBeVisible(value);
+    async buttonisActiveValidation(element){
+        try {
+            if (!this.page) {
+                throw new Error('Page object is not defined.');
+            }
+            await expect(this.page.locator(element)).toBeEnabled();
+        } catch (error) {
+            console.error('Error validating image:', error);
+        }
     }
+
+    
 }
